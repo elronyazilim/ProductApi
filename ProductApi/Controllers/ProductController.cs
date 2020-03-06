1e12f2dc3b1f43ad78385eef24c6edd6a5076ce3
elronyazilim <55488502+elronyazilim@users.noreply.github.com>
2020-03-06 13:32:57 +0300

    first commit

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Ürün eklendi";
+        }
+    }
+}
